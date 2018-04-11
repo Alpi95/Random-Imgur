c4f69cceca25097069e812f4daf3cd3852f0167b
Alpi95 <aleksander.pietrzak95@gmail.com>
2018-04-11 13:54:35 +0200

    New char list

--- RandomImgur/Program.cs
+++ RandomImgur/Program.cs
@@ -15,8 +15,8 @@ namespace RandomImgur
         private static readonly Random rnd = new Random();
         private static string url = "https://i.imgur.com/";
 
-        //static private string[] charList = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-        static private string[] charList = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        static private string[] charList = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        //static private string[] charList = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
         static private string RandomCharOrNmb()
         {
